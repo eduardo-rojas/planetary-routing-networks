d7c4df01590fe2a403837e3d37978c9d37812f09
Carlos Hoyos <carlos.edhr@comunidad.unam.mx>
2020-02-26 17:41:42 -0800

    Set up folder structure and spring boot back end

---
+++ PlanetaryRoutingBackend/src/main/java/me/carlosehr/planetaryrouting/PlanetaryroutingApplication.java
@@ -0,0 +1,13 @@
+package me.carlosehr.planetaryrouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetaryroutingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanetaryroutingApplication.class, args);
+    }
+
+}
---
+++ PlanetaryRoutingBackend/src/test/java/me/carlosehr/planetaryrouting/PlanetaryroutingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.carlosehr.planetaryrouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetaryroutingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
